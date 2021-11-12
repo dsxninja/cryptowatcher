b53328ff21cda55e9c3161c558eabc91d26484f6
bo-bic-laki <nikita134222@gmail.com>
2021-11-12 18:39:36 +0300

    Hotfixed (module parser)

--- modules/parser.js
+++ modules/parser.js
@@ -6,6 +6,8 @@ module.exports = async (name) => {
     let $ = cheerio.load(data, { decodeEntities: false });
 
     return {
-        price: $('.sc-16r8icm-0.kjciSH.priceTitle .priceValue').text()
+        price: $('.sc-16r8icm-0.kjciSH.priceTitle .priceValue').text(),
+        percent: $('.sc-16r8icm-0.kjciSH.priceTitle .sc-15yy2pl-0').text(),
+        percent_pos: $('.sc-16r8icm-0.kjciSH.priceTitle .sc-15yy2pl-0 .icon-Caret-down').length > 0 ? 'down' : 'up'
     }
 }
