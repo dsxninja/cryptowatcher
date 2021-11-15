dd5e4d1637447c8688f500fd958e3a61d029c6b1
webgtx <dxv1d@tuta.io>
2021-11-15 20:20:38 +0200

    Revert to "Hotfixed (module parser)"
    This reverts commit b53328ff21cda55e9c3161c558eabc91d26484f6.

--- modules/parser.js
+++ modules/parser.js
@@ -6,8 +6,6 @@ module.exports = async (name) => {
     let $ = cheerio.load(data, { decodeEntities: false });
 
     return {
-        price: $('.sc-16r8icm-0.kjciSH.priceTitle .priceValue').text(),
-        percent: $('.sc-16r8icm-0.kjciSH.priceTitle .sc-15yy2pl-0').text(),
-        percent_pos: $('.sc-16r8icm-0.kjciSH.priceTitle .sc-15yy2pl-0 .icon-Caret-down').length > 0 ? 'down' : 'up'
+        price: $('.sc-16r8icm-0.kjciSH.priceTitle .priceValue').text()
     }
 }
